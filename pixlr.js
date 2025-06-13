a90f5e1265e45a6560e6495e45394c56bfd8794b
oopfff <uhoh.yip@gmail.com>
2025-06-12 20:06:00 -0400

    Update pixlr.js

--- pixlr.js
+++ pixlr.js
@@ -1,3 +1,4 @@
+// this will reset your daily project limit
 alert("WARNING: This will remove all current projects!");
 // clear local storage
 localStorage.clear(),sessionStorage.clear(),indexedDB.databases().then(e=>{e.forEach(e=>{indexedDB.deleteDatabase(e.name)})}),document.cookie.split(";").forEach(e=>{let a=e.indexOf("="),$=a>-1?e.substr(0,a):e;document.cookie=$+"=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/"});
