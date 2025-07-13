d6cbdb2f74ecd6927c696f8a7b33e388843861da
oopfff <uhoh.yip@gmail.com>
2025-07-13 09:44:09 -0400

    Create tiktok.js

---
+++ tiktok.js
@@ -0,0 +1,6 @@
+// free shares
+for (let index = 0; index < 100; index++) {
+    document.getElementsByClassName("css-vzi0wx-DivAnimationContainer er2ywmz9")[3].click();
+    document.getElementsByClassName("css-1t5edl0-DivActionContainer e20e1og1")[2].click();
+    console.log("Successfully copied " + index + " times");
+}
